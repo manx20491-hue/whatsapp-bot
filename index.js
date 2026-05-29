02d2ad406405f34da4fd8fe777c14f918e113d9a
manx20491-hue <manx20491@gmail.com>
2026-05-30 01:04:16 +0530

    Add initial WhatsApp bot index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('WhatsApp bot started');
+
+// Basic WhatsApp bot starter file
+// You can later integrate Baileys or whatsapp-web.js
+
+function startBot() {
+    console.log('Bot is running...');
+}
+
+startBot();
